ad08c01d9997903f233403ae8b44349d646017e1
Geoff Fox <fox.geoffrey@gmail.com>
2021-05-13 21:26:30 -0700

    FCC - Create a Complex JSX Element

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 /* VSCode example */
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
+var element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello, world!"
+);
+ReactDOM.render(element, document.getElementById("root"));
 
 /* FCC - Create a Simple JSX Element */
-const JSX = <h1>Hello JSX!</h1>
-ReactDOM.render(JSX, document.getElementById('root'))
+const JSX = <h1>Hello JSX!</h1>;
+ReactDOM.render(JSX, document.getElementById("root"));
 
-
+/* FCC - Create a Complex JSX Element */
+const JSX2 = (
+  <div>
+    <h1>Paragraph One</h1>
+    <p>Paragraph Two</p>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+    </ul>
+  </div>
+  );
+  ReactDOM.render(JSX2, document.getElementById("root"));
 
 /*
 ReactDOM.render(
