f3ed69be623f6e27d6cb4ef35e7a68ecd4a32f77
Geoff Fox <fox.geoffrey@gmail.com>
2021-05-13 21:10:00 -0700

    FCC -Create a Simple JSX Element

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+/* VSCode example */
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 ReactDOM.render(element, document.getElementById('root'));
+
+/* FCC - Create a Simple JSX Element */
+const JSX = <h1>Hello JSX</h1>
+ReactDOM.render(JSX, document.getElementById('root'))
+
+
 
 /*
 ReactDOM.render(
