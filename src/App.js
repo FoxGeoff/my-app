b070ebe8913912e39c030cc76c108479fac44cac
Geoff Fox <fox.geoffrey@gmail.com>
2021-05-18 17:50:56 -0700

    Add JXS Person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // import logo from './logo.svg';
+import Person from './Person/Person';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <h1>Hi , I'm a React App!</h1>
+       <p>This is really working!</p>
+       <button>Switch Name</button>
+       <Person name="Max" age="28" />
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+  
+ const person = () => {
+     return <p> I'm a Person</p>
+ };
+
+ export default person;
